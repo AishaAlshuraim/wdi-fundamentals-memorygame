80f37df2a9a6206cfa4c988f2c8c746b958bb9ee
aisha <aishaalshuraim@gmail.com>
2019-06-03 16:02:26 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,24 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne= cards[0];
-var cardTow = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTow);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTow);
-if (cardsInPlay.length === 2)
-{
-if ( cardsInPlay[0]===cardsInPlay[1]){
-	alert("You found a match!");
+
+function checkForMatch(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
 }
-	else{
-    alert("Sorry, try again.");
-	} 
 }
+
+function flipCard(cardId){
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push( cards[cardId]);
+if (cardsInPlay.length === 2){
+	checkForMatch();
+}
+
+
+ }
+ flipCard(0);
+ flipCard(2);
 
 
